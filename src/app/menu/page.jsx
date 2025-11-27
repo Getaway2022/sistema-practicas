21a048e54af449ba568c389639955fddaf72dcd9
Junior <juniorgamio@gmail.com>
2025-11-26 19:42:43 -0500

    Fix: Update authOptions imports in page.jsx files

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from '@/lib/auth';
 import Link from "next/link";
 import { PrismaClient } from "@prisma/client";
 import OportunidadCard from "./OportunidadCard";
