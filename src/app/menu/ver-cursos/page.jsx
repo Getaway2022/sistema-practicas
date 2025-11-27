cd015b008860cbf2ac4a5e37aab00f994e6fa65c
Junior <juniorgamio@gmail.com>
2025-11-26 19:13:48 -0500

    Fix: Update authOptions imports and add critters

--- src/app/menu/ver-cursos/page.jsx
+++ src/app/menu/ver-cursos/page.jsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../api/auth/[...nextauth]/route";
+import { authOptions } from '@/lib/auth';
 import { PrismaClient } from "@prisma/client";
 import VerCursosClient from "./VerCursosClient";
 
